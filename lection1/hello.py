2b2bed12e02392bfe62708adfcb52dcc1be663bb
DanilaPestov <89147750075@mail.ru>
2022-07-16 02:42:53 +1000

    seminar1

---
+++ lection1/hello.py
@@ -0,0 +1,45 @@
+#print('hello world')
+
+#a = 123
+#b = 1.23
+# print(type(a))
+# print(a)
+# print(type(b))
+# print(b)
+
+#value = None
+#value = 12334
+# print(type(value))
+# print(value)
+
+#s = 'hello \nworld'
+#s = 'hello world'
+# print(type(s))
+# print(s)
+
+#print(a, '-', b, '-', s)
+#print('{} - {} - {}'.format(a, b, s))
+#print(f'{a} - {b} - {s}')
+
+#f = False
+# print(f)
+
+#list = [1, 2, 3]
+# print(list)
+
+#print('введите c')
+# c = input()
+# print('введите d')
+# d = input()
+# print(f'{c} {d}')
+
+# print('введите c')
+# c = int(input())
+#print('введите d')
+#d = int(input())
+#print(c, '+', d, '=', c+d)
+
+a = 1.3
+b = 3
+c = round(a*b)
+print(c)
